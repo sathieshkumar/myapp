507934dfa3fd0c03000d7d7d5ba2a119be56bb9a
Sathiesh kumar Murugan <msathiesh@athenahealth.com>
2018-08-08 12:51:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var port = 8081;
+var app = express();
+app.listen(port);
+
+app.get('/book/:id', (request, response) => {
+    let bookid = request.params.id;
+    response.send(`Got Book ${bookid} !!!`);
+});
